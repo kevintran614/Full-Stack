acbf8ee22ec9f2c5047b40d604fc5fb13cbac0a1
Kevin Tran <110150247+kevintran614@users.noreply.github.com>
2024-05-21 23:30:13 -0700

    reviewing js syntax

---
+++ js-examples/example.js
@@ -0,0 +1,15 @@
+console.log(typeof 4);
+
+let someName = 10;
+
+console.log(someName);
+
+let score = 0;
+score += 5;
+
+console.log(score);
+
+let myBoolVar = true;
+
+console.log(myBoolVar);
+console.log(typeof myBoolVar);
