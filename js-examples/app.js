63f3d32e917e080c52433b838c4e659d665e6fe6
Kevin Tran <110150247+kevintran614@users.noreply.github.com>
2024-05-23 22:32:59 -0700

    reviewing javascript syntax pt. 2

---
+++ js-examples/app.js
@@ -0,0 +1,14 @@
+console.log("Hello Kevin!");
+
+if (1 + 1 === 2) {
+  console.log("1 + 1 == 2!");
+}
+
+let random = Math.random();
+
+if (random < 0.5) {
+  console.log("< 0.5!");
+}
+
+const password = prompt("enter a password");
+console.log(password);
