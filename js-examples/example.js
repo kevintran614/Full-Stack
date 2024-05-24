9fe10ef11720b5aa7b635aeca8f9596238428f21
Kevin Tran <110150247+kevintran614@users.noreply.github.com>
2024-05-23 21:59:06 -0700

    reviewing js syntax

--- js-examples/example.js
+++ js-examples/example.js
@@ -13,3 +13,16 @@ let myBoolVar = true;
 
 console.log(myBoolVar);
 console.log(typeof myBoolVar);
+
+let strAnimal = "animal";
+console.log(strAnimal[3]);
+console.log(strAnimal.length);
+
+console.log("bob" + " is a billy");
+
+const myName = "kevin";
+console.log(myName.slice(1, 3));
+
+const exNum = 5;
+const myStr = `You bought ${exNum} breads`;
+console.log(myStr);
