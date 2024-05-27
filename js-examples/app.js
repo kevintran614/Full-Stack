15a0ac0ce504749e528f4388a75e87bde32959a3
Kevin Tran <110150247+kevintran614@users.noreply.github.com>
2024-05-27 12:49:38 -0700

    reviewed function syntax

--- js-examples/app.js
+++ js-examples/app.js
@@ -1,18 +1,3 @@
-console.log("Hello Kevin!");
-
-if (1 + 1 === 2) {
-  console.log("1 + 1 == 2!");
-}
-
-let random = Math.random();
-
-if (random < 0.5) {
-  console.log("< 0.5!");
-}
-
-const password = prompt("enter a password");
-console.log(password);
-
 // ------- string formatting -------
 let myName = "Bob";
 console.log(` Hi, ${myName}!`);
@@ -63,14 +48,128 @@ const testScores = {
   Brian: 100,
 };
 
+/* this is similar to python syntax */
 for (let person in testScores) {
   console.log(person);
   console.log(testScores[person]);
 }
 
 // ------- functions -------
+/* in javascript, we declare functions using "function" keyword vs. int, bool, etc. like in C++*/
 function greet(person) {
   console.log(` Hi, ${person}!`);
 }
 
 greet("Kevin");
+
+/*
+instead of doing:
+
+        int add(x, y) {
+          return x + y;
+        }
+
+we can do:
+
+        function add(x ,y) {
+          return x + y;
+        }
+*/
+
+// ------- function expressions -------
+function add1(x, y) {
+  return x + y;
+}
+
+// you can store a function in a variable
+const add2 = function (x, y) {
+  return x + y;
+};
+
+// you can pass a function as an argument
+function callTwice(func) {
+  func();
+  func();
+}
+
+function printName() {
+  console.log("Kevin is my first name");
+}
+
+callTwice(printName);
+
+// you can return a function within a function
+function makeMysteryFunc() {
+  const rand = Math.random();
+
+  if (rand > 0.5) {
+    return function () {
+      alert("YOU ARE A GOOD FUNCTION");
+    };
+  } else {
+    return function () {
+      alert("YOU ARE A BAD FUNCTION");
+    };
+  }
+}
+
+makeMysteryFunc();
+
+// why should we return a function from a function?
+function isBetween1(num) {
+  return num >= 50 && num <= 100;
+}
+
+function isBetween2(num) {
+  return num >= 1 && num <= 10;
+}
+
+isBetween1(65);
+isBetween2(5);
+
+/* but what if we don't want to hardcode (50, 100) and (1, 10) in our functions? */
+/* answer: we can return a function from a function! */
+
+function isBetween3(min, max) {
+  return function (num) {
+    return num >= min && num <= max;
+  };
+}
+
+const returnOfIsBetween3 = isBetween3(500, 1000);
+// what returnOfIsBetween3 now holds is:
+
+/*
+            return function (num) {
+              return num >= min && num <= max;
+            };
+*/
+
+// where min = 500 and max = 1000
+
+returnOfIsBetween3(600);
+
+// ------- methods -------
+/* a method is a property of an object that is a function */
+
+const math = {
+  /* multiply and divide are properties of the math object */
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+};
+
+math.multiply(5, 5);
+math.divide(20, 4);
+
+// ------- this keyword -------
+const cat = {
+  name: "Kevin Tran",
+  color: "Green",
+  meow: function () {
+    console.log(this.name);
+  },
+};
