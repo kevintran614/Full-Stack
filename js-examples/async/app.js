1d6384b18f000c8dd8d49c340d3bfb36d72c757f
Kevin Tran <110150247+kevintran614@users.noreply.github.com>
2024-06-30 01:00:20 -0700

    Worked on async, await, promises

--- js-examples/async/app.js
+++ js-examples/async/app.js
@@ -1,15 +1,48 @@
-const request = requestPromise("http://www.google.com");
+/* async functions */
 
-/* request can either contain a promise that is resolved or unresolved */
-
-/*
-      1) if promise is resolved: then() is called
-      2) else: then() is not called, but catch() is
+/* 
+        1. Automatically returns a Promise
+        2. If there is an error in our async function, the promise will return a reject()
+        3. Else, the promise will be resolved with whatever is returned in our async function
 */
-request
-  .then(() => {
-    console.log("Success");
+
+const sing = async () => {
+  //   throw "OH NO";
+  return "LA LA LA LA";
+};
+
+sing()
+  .then((data) => {
+    console.log("Promise resolved with:", data); // ex: data = "LA LA LA LA"
   })
-  .catch(() => {
-    console.log("Failed");
+  .catch((err) => {
+    console.log("Promise rejected:", err); // err = "OH NO"
   });
+
+/* await */
+
+/* 
+        1. Makes the code synchronous --> executes line by line
+        2. Will wait for one code to completely execute before proceeding with next line of code
+        3. await must be used in conjunction with async
+        4. WAITS until a promise is resolved before proceeding with next line.
+*/
+
+async function test1() {
+  await sing();
+  console.log("sing 1");
+  await sing();
+  console.log("sing 2");
+  await sing();
+  console.log("sing 3");
+}
+
+/* error handling in async functions */
+async function test2() {
+  try {
+    let data1 = await sing();
+    console.log(data1); // only if resolved
+  } catch (error) {
+    console.log("Caught an error:", error); // only if rejected
+  }
+}
