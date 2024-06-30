839439f46b87b19438babca211ca02de7770ca43
Kevin Tran <110150247+kevintran614@users.noreply.github.com>
2024-06-30 11:07:56 -0700

    worked on ajax, http requests, http headers

---
+++ js-examples/ajax/app.js
@@ -0,0 +1,15 @@
+/* ajax */
+
+/*
+        1. ajax = making request behind the scenes.
+        2. a = asynchronous, j = javascript, a = and, x = xml/json.
+        3. you can use JSON.parse(JSON object) to convert JSON to a javascript object.
+        4. you can use hoppscotch or postman to mimic HTTP requests without creating a frontend.
+*/
+
+/* http verbs/status codes */
+
+/*
+        1. GET = receive data, POST = save data on server, DELETE
+        2. 200 = OK, 400 = error
+*/
