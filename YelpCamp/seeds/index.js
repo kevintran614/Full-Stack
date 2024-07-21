ac678c66f26818aa277753cf511d3bba4566bd86
Kevin Tran <110150247+kevintran614@users.noreply.github.com>
2024-07-20 22:57:57 -0700

    added bootstrap for campground views

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -20,10 +20,15 @@ const seedDB = async () => {
 
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
 
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia praesentium voluptates aut nesciunt deserunt ipsa fuga consequuntur cupiditate quia repellat. Unde rem dignissimos voluptates officia earum accusantium doloribus nulla? Autem?",
+      price,
     });
 
     await camp.save();
