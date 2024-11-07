27174885a1581570e92b45891d79b93c09cecd4b
Kevin Tran <kevinat1@uci.edu>
2024-11-06 16:51:37 -0800

    added jwt token, login route, logout route

--- Chat-App/backend/controllers/auth.controller.js
+++ Chat-App/backend/controllers/auth.controller.js
@@ -3,6 +3,7 @@
 ///////////////////////////////////////////////////////////////////////////////////////
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -34,6 +35,9 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
+      // generte jwt token
+      generateTokenAndSetCookie(newUser._id, res);
+
       await newUser.save();
 
       res.status(201).json({
@@ -52,16 +56,45 @@ export const signup = async (req, res) => {
 
     res
       .status(500)
-      .json({ error: "Intern Server Error: could not signup user" });
+      .json({ error: "Internal Server Error: could not signup user" });
   }
 };
 
-export const login = (req, res) => {
-  res.send("Login user");
-  console.log("Login user");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+
+    res.status(500).json({ error: "Internal Server Error: could not login" });
+  }
 };
 
 export const logout = (req, res) => {
-  res.send("Logout user");
-  console.log("Logout user");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+
+    res.status(500).json({ error: "Internal Server Error: could not logout" });
+  }
 };
