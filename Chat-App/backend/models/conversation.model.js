c7db8bcd4fb4e6b7511ad6db8377a1caa802b141
Kevin Tran <kevinat1@uci.edu>
2024-11-11 09:26:29 -0800

    added convesation and model between two users, added send message route

--- Chat-App/backend/models/conversation.model.js
+++ Chat-App/backend/models/conversation.model.js
@@ -21,4 +21,4 @@ const conversationSchema = new mongoose.Schema(
 
 const Conversation = mongoose.model("Conversation", conversationSchema);
 
-export default Converastion;
+export default Conversation;
--- Chat-App/backend/server.js
+++ Chat-App/backend/server.js
@@ -4,16 +4,22 @@
 
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 dotenv.config();
+
 app.use(express.json()); // to parse incoming requests (from req.body from auth.controller.js)
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
